26977360516d346f51bf26e7bb853e9a00438cf9
Dnayir <nayir_da@hotmail.co.uk>
2022-10-22 00:02:09 +0100

    player starting position

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const image = new Image()
 image.src = './img/Pokemon Style Game Map.png'
 
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
+
 image.onload = () => {
-    c.drawImage(image, -840, -170)
+    c.drawImage(image, -1000, -225)
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        130,
+        390,
+        playerImage.width / 4,
+        playerImage.height
+        )
 }
+
