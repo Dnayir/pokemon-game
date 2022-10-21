9bd08b2ea329dd7d308a8a9dbadb8edbfa02c39a
Dnayir <nayir_da@hotmail.co.uk>
2022-10-22 00:27:10 +0100

    classified background

--- index.js
+++ index.js
@@ -13,8 +13,27 @@ image.src = './img/Pokemon Style Game Map.png'
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
-image.onload = () => {
-    c.drawImage(image, -1000, -225)
+class Sprite {
+    constructor({position,velocity, image}) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, -1000, -225)
+    }
+}
+
+const background = new Sprite({position: {
+    x: -1000,
+    y: -225
+    },
+    image: image
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
     c.drawImage(
         playerImage,
         0,
@@ -27,4 +46,21 @@ image.onload = () => {
         playerImage.height
         )
 }
+animate()
 
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            console.log('pressed w key')
+        break
+        case 'a':
+            console.log('pressed a key')
+        break
+        case 's':
+            console.log('pressed s key')
+        break
+        case 'd':
+            console.log('pressed d key')
+        break
+    }
+})
