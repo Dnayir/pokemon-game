b086b1bf17e4b0e7d027ba4f1c78ca81d0583bcc
Dnayir <nayir_da@hotmail.co.uk>
2022-10-22 00:55:53 +0100

    player movement added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class Sprite {
     }
 
     draw() {
-        c.drawImage(this.image, -1000, -225)
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
@@ -30,6 +30,21 @@ const background = new Sprite({position: {
     },
     image: image
 })
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -45,22 +60,48 @@ function animate() {
         playerImage.width / 4,
         playerImage.height
         )
+        if (keys.w.pressed && lastKey === 'w') background.position.y += 2
+        else if (keys.a.pressed && lastKey === 'a') background.position.x += 2
+        else if (keys.s.pressed && lastKey === 's') background.position.y -= 2
+        else if (keys.d.pressed && lastKey === 'd') background.position.x -= 2
 }
 animate()
 
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
-            console.log('pressed w key')
+            keys.w.pressed = true
+            lastKey = 'w'
         break
         case 'a':
-            console.log('pressed a key')
+            keys.a.pressed = true
+            lastKey = 'a'
         break
         case 's':
-            console.log('pressed s key')
+            keys.s.pressed = true
+            lastKey = 's'
         break
         case 'd':
-            console.log('pressed d key')
+            keys.d.pressed = true
+            lastKey = 'd'
+        break
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+        break
+        case 'a':
+            keys.a.pressed = false
+        break
+        case 's':
+            keys.s.pressed = false
+        break
+        case 'd':
+            keys.d.pressed = false
         break
     }
 })
